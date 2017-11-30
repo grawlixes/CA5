3b7988a7092260f94c1be0e82c7288cd9ec7549c
Leandro Ribeiro <lribeir1@cs.binghamton.edu>
2017-11-30 01:28:54 -0500

    Did some file reading

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
-#includ <fstream>
+#include <fstream>
+//#include <hash_map>
+#include "RequirementsLL.h"
 
-int main(int argc, char ** argv) {
+using namespace std;
+//using namespace __gnu_cxx;
+
+int main(int argc, char ** argv){
+	string line;
+	int credLimit;
+	//hash_map<const string, int, hash<const char*>, eqstr> credTags;
+	ifstream reqFile(argv[1]);
+	if(reqFile.is_open()){
+		while(getline(reqFile,line,' ')){
+			cout << line << endl;
+			if(line == "TOTAL"){
+				getline(reqFile,line,' ');
+				credLimit = stoi(line);
+			}
+			if(line == "CREDIT"){
+				//string credLetter;
+				//getline(reqFile,credLetter,' ');
+				//getline(reqFile,line,' ');
+				//cout << "credTags[" << credLetter << "] = " << line << endl;
+				//credTags[credLetter] = stoi(line)
+			}
+		}
+		reqFile.close();
+	}else cout << "Unable to open " << argv[1] << endl;
+	//hash_map<const string, int, hash<const char*>, eqstr> classes;
 }

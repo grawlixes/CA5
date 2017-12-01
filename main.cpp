fa32e2a734d4c2cec0343c746480c382ca84b46a
Leandro Ribeiro <lribeir1@cs.binghamton.edu>
2017-12-01 15:59:06 -0500

    Saved tags and total credits

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <fstream>
-//#include <hash_map>
+#include <unordered_map>
 #include "RequirementsLL.h"
 
 using namespace std;
@@ -8,24 +8,20 @@ using namespace std;
 int main(int argc, char ** argv){
 	string line;
 	int credLimit;
-	//hash_map<const string, int, hash<const char*>, eqstr> credTags;
+	unordered_map<string, int> credTags;
 	ifstream reqFile(argv[1]);
 	if(reqFile.is_open()){
-		while(getline(reqFile,line,' ')){
-			cout << line << endl;
-			if(line == "TOTAL"){
-				getline(reqFile,line,' ');
-				credLimit = stoi(line);
+		while(getline(reqFile,line)){
+			cout <<"start" << line << "end"<< endl;
+			if(line.substr(0,5) == "TOTAL"){
+				credLimit = stoi(line.substr(6,3));
+				cout << "credlimit: " << credLimit << endl;
 			}
-			if(line == "CREDIT"){
-				//string credLetter;
-				//getline(reqFile,credLetter,' ');
-				//getline(reqFile,line,' ');
-				//cout << "credTags[" << credLetter << "] = " << line << endl;
-				//credTags[credLetter] = stoi(line)
+			if(line.substr(0,6) == "CREDIT"){
+				cout << "credTags[" << line.substr(7,1) << "] = " << line.substr(9,3) << endl;
+				credTags[line.substr(7,1)] = stoi(line.substr(9,3));
 			}
 		}
 		reqFile.close();
 	}else cout << "Unable to open " << argv[1] << endl;
-	//hash_map<const string, int, hash<const char*>, eqstr> classes;
 }
--- makefile
+++ makefile
@@ -7,7 +7,7 @@ all:	$(TARGET)
 # Specify the object files that the target depends on
 # Also specify the object files needed to create the executable
 $(TARGET):	main.o
-	g++  main.o RequirementsLL.o $(TARGET) 
+	g++  main.o RequirementsLL.o -o $(TARGET) 
 
 # Specify how the object files should be created from source files
 main.o:	main.cpp

38fdc510f344228912d3c616783f033eb05a7ec7
Leandro Ribeiro <lribeir1@cs.binghamton.edu>
2017-12-06 19:45:14 -0500

    Added requirements to graph

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include "RequirementsLL.h"
 
 using namespace std;
-//using namespace __gnu_cxx;
 
 unordered_map<string, string> create_offerings_map();
 
@@ -15,7 +14,6 @@ int main(int argc, char ** argv){
 	ifstream reqFile(argv[1]);
 	if(reqFile.is_open()){
 		while(getline(reqFile,line)){
-			//cout <<"start" << line << "end"<< endl;
 			if(line.substr(0,5) == "TOTAL"){
 				credLimit = stoi(line.substr(6,3));
 				cout << "credlimit: " << credLimit << endl;
@@ -23,21 +21,28 @@ int main(int argc, char ** argv){
 				cout << "credTags[" << line.substr(7,1) << "] = " << line.substr(9,3) << endl;
 				credTags[line.substr(7,1)] = stoi(line.substr(9,3));
 			}else if(line.substr(0,6) == "COURSE"){
+				RequirementsLL reqLL;
+				reqLL.set_first_requirement(line.substr(7,5),line.substr(13,1));
 				cout << line.substr(7,5) << " added to graph." << endl;
 				int classIdx = 15;
 				if(line.substr(13,1) == "R"){
 					while(classIdx < line.length()){
+						reqLL.insert_requirement(line.substr(classIdx,5),line.substr(13,1));
 						cout << line.substr(classIdx,5) << " is a prereq" << endl;
 						classIdx += 6;
 					}
 				}else if(line.substr(13,1) == "O"){
 					while(classIdx < line.length()){
+						reqLL.insert_requirement(line.substr(classIdx,5),line.substr(13,1));
 						cout << line.substr(classIdx,5) << " is a prereq" << endl;
 						classIdx += 6;
 					}
 				}else if(line.substr(13,1) == "M"){
 					cout << line.substr(7,5) << " is mandatory!" << endl;
 				}
+				reqGraph.push_back(reqLL);
+			}else if(line.substr(0,6) == "CHOOSE"){
+			//This still needs to be done
 			}
 		}
 		reqFile.close();
